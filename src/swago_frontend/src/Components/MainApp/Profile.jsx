df32aacd86389395f8b6f35012103c4c30744724
k4p1l <k4p1ll.23@gmail.com>
2025-01-06 11:04:43 +0000

    final profile changes

--- src/swago_frontend/src/Components/MainApp/Profile.jsx
+++ src/swago_frontend/src/Components/MainApp/Profile.jsx
@@ -15,17 +15,21 @@ export const Profile = () => {
     <div className="w-full min-h-screen text-white bg-[#101a23]">
       <MainNavbar />
       <div className="">
-        <div className="flex items-center mt-12 px-12 gap-20">
-          <img
-            src={pfp}
-            alt=""
-            className="w-[250px] h-[250px] object-cover rounded-full"
-          />
-          <div className="flex flex-col gap-6">
-            <h1 className="text-4xl font-semibold mt-2">Yogita</h1>
-            <p className="text-[#c1c1c1] bg-[#354A63] px-4 py-2 rounded-3xl">
-              {principal ? principal.toText() : "No Principal"}
-            </p>
+        <div className="flex items-center justify-between mt-12 px-12 ">
+          <div className="flex items-center gap-20">
+            <img
+              src={pfp}
+              alt=""
+              className="w-[250px] h-[250px] object-cover rounded-full"
+            />
+            <div className="flex flex-col gap-6">
+              <h1 className="text-4xl font-semibold mt-2">Yogita</h1>
+              <p className="text-[#c1c1c1] bg-[#354A63] px-4 py-2 rounded-3xl">
+                {principal
+                  ? principal.toText().slice(0, 11) + "..."
+                  : "No Principal"}
+              </p>
+            </div>
           </div>
           <button className="px-8 py-2 border-2 rounded-3xl border-[#ffffff]">
             Edit Profile
@@ -33,10 +37,10 @@ export const Profile = () => {
         </div>
         <div className="flex justify-center items-center gap-20 mt-8">
           <RouterLink to="/transaction-history">
-            <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white">
-              <div className="w-[90px] h-[90px] bg-[#ffffff] rounded-full flex justify-center items-center">
+            <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white border-2 border-[#ffffff]">
+              <div className="w-[90px] h-[90px] bg-[#ffffff] rounded-full flex justify-center items-center ">
                 <img
-                  className="w-[38px] h-[38px]"
+                  className="w-[32px] h-[32px]"
                   src={transactionIcon}
                   alt=""
                 />
@@ -44,25 +48,25 @@ export const Profile = () => {
               <p>Transaction History</p>
             </div>
           </RouterLink>
-          <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white">
+          <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white border-2 border-[#ffffff]">
             <div className="w-[90px] h-[90px] bg-[#ffffff] rounded-full flex justify-center items-center">
-              <img src={eventsIcons} className="w-[38px] h-[38px]" alt="" />
+              <img src={eventsIcons} className="w-[32px] h-[32px]" alt="" />
             </div>
             <p>Events Participated</p>
           </div>
-          <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white">
+          <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white border-2 border-[#ffffff]">
             <div className="w-[90px] h-[90px] bg-[#ffffff] rounded-full flex justify-center items-center">
               <img
                 src={eventsCreatedIcon}
-                className="w-[38px] h-[38px]"
+                className="w-[32px] h-[32px]"
                 alt=""
               />
             </div>
             <p>Events Created</p>
           </div>
-          <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white">
+          <div className="w-[200px] h-[200px]  rounded-lg flex justify-center items-center flex-col gap-4 text-white border-2 border-[#ffffff]">
             <div className="w-[90px] h-[90px] bg-[#ffffff] rounded-full flex justify-center items-center">
-              <img src={leaderboardIcon} className="w-[38px] h-[38px]" alt="" />
+              <img src={leaderboardIcon} className="w-[32px] h-[32px]" alt="" />
             </div>
             <p>Leaderboard</p>
           </div>
